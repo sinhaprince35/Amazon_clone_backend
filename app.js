7394a17c97908e1e39737e0c7f6c469f8c453a6c
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-10-18 06:54:32 +0530

    Finish

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8005;
